171da1469aa806e1611d50baee703a2b2142d85d
Darryl Pogue <darryl@ayogo.com>
2023-12-28 15:40:59 -0800

    Initial commit of wpt-test module

---
+++ README.md
@@ -0,0 +1,24 @@
+WPT Test Runner
+===============
+
+NodeJS test runner for web-platform-test testharness tests.
+
+Contributing
+------------
+
+Contributions of bug reports, feature requests, and pull requests are greatly
+appreciated!
+
+Please note that this project is released with a [Contributor Code of
+Conduct](CODE_OF_CONDUCT.md). By participating in this project you agree to
+abide by its terms.
+
+
+Licence
+-------
+
+Test runner released under the [MIT Licence](LICENCE).
+Web Platform Test testharness.js and related files released under the
+[3-Clause BSD Licence](https://github.com/web-platform-tests/wpt/blob/master/LICENSE.md).
+
+Copyright © 2023 Ayogo Health Inc.
---
+++ wpt-test-cli.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+/*! Copyright (c) 2023 Ayogo Health Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+import process from 'node:process';
+import reporters from "node:test/reporters";
+import { parseArgs } from "node:util";
+import { runServer } from "./lib/wpt_server.js";
+import run from "./lib/testrunner.js";
+
+const flagOptions = {
+  help: {
+    short: "h",
+    type: "boolean"
+  },
+  serve: {
+    short: "s",
+    type: "boolean"
+  },
+  server: {
+    type: "boolean"
+  },
+  verbose: {
+    short: "v",
+    type: "boolean"
+  },
+  browser: {
+    type: "string"
+  },
+  reporter: {
+    type: "string"
+  }
+};
+
+const opts = {};
+const args = parseArgs({ strict: true, allowPositionals: true, options: flagOptions });
+
+if ("help" in args.values) {
+  console.log("TODO: Write help output");
+  process.exit(0);
+}
+
+if ("verbose" in args.values) {
+  opts.verbose = true;
+}
+
+if ("browser" in args.values) {
+  opts.browser = args.values.browser;
+}
+
+const testReporter = reporters.spec;
+if ("reporter" in args.values) {
+  if (args.values.reporter in reporters) {
+    testReporter = reporters[args.values.reporter];
+  }
+}
+
+if ("serve" in args.values || "server" in args.values) {
+  const shutdown = await runServer();
+
+  process.on("SIGINT", shutdown)
+  process.on("SIGTERM", shutdown)
+  process.on("SIGQUIT", shutdown)
+} else {
+  const testcases = [
+      "http://localhost:3000/test/test.html"
+  ];
+
+  const test_stream = run(testcases, opts);
+  test_stream.compose(testReporter).pipe(process.stdout);
+
+  await test_stream.waitUntilDone();
+
+  setImmediate(() => {
+    console.log("");
+    process.exit(test_stream.status);
+  });
+}
